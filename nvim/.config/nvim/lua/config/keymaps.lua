7e76d3f072dedc6b4ca9b06c3987a7f6b06e53ac
kznLeaf <kunpengdu43@gmail.com>
2026-04-22 22:43:51 +0800

    nvim: add two autocmds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,29 @@ vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search 
 -- vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 -- vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>w", "<cmd>close<CR>", { desc = "Command 'close'" }) -- only close current split window, Keep the original window
+
+----------------------autocmds-------------------------
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "DiffText", timeout = 250 })
+	end,
+})
+
+-- Keymap to copy current diagnostics to the system clipboard
+vim.keymap.set("n", "<leader>dy", function()
+	local current_word = vim.fn.expand("<cword>") -- Get the word under the cursor. E.g. vim
+	local diagnostics = vim.diagnostic.get(0) -- Get diagnostics for the current buffer.
+	local diagnostic_messages = {}
+	for _, diagnostic in ipairs(diagnostics) do
+		if string.find(diagnostic.message, current_word) then
+			table.insert(diagnostic_messages, diagnostic.message)
+		end
+	end
+	local diagnostic_text = table.concat(diagnostic_messages, "\n")
+	vim.fn.setreg("+", diagnostic_text) -- Copy to system clipboard
+	print("Diagnostics for '" .. current_word .. "' copied to clipboard")
+end, { remap = false, desc = "Copy diagnostics to clipboard" })
